c13d6640bcf34d5ea5d94b1914354aa79ba175f2
Ev Zem <jekh.zem@gmail.com>
2025-05-02 17:06:00 +0300

    попытка 2

--- api_yamdb/api_yamdb/api/serializers.py
+++ api_yamdb/api_yamdb/api/serializers.py
@@ -97,14 +97,12 @@ class TokenSerializer(serializers.Serializer):
 
 
 class GenreSerializer(serializers.ModelSerializer):
-
     class Meta:
         fields = ['name', 'slug']
         model = Genre
 
 
 class CategorySerializer(serializers.ModelSerializer):
-
     class Meta:
         fields = ['name', 'slug']
         model = Category
