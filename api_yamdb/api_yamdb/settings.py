18367f859efa5cad17f60deb81d7f3f85684c372
Ev Zem <jekh.zem@gmail.com>
2025-05-03 17:34:10 +0300

    Попытка 11...

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -75,23 +75,6 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'users.User'
 
-# DRF settings
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-        'rest_framework.filters.OrderingFilter',
-    ]
-}
-
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
