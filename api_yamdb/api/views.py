4e2068c41d5e4d69c1e155128bd2b9a59c8ee101
Ev Zem <jekh.zem@gmail.com>
2025-05-03 16:28:39 +0300

    Попытка 7...

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,16 @@
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, pagination, views, viewsets
+
+from rest_framework import mixins, pagination, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework_simplejwt.tokens import AccessToken
 
 from reviews.models import Review
@@ -13,12 +18,12 @@ from titles.models import Category, Genre, Title
 from users.models import User
 from users.permissions import (IsAdmin, IsAdminOrReadOnly,
                                IsAuthorModeratorAdminOrReadOnly)
-
 from .filters import TitleFilter
-from .serializers import (CategorySerializer, CommentSerializer,
-                          GenreSerializer, ReviewSerializer, SignUpSerializer,
-                          TitleGETSerializer, TitleSerializer, TokenSerializer,
-                          UserSerializer)
+from .serializers import (
+    CategorySerializer, CommentSerializer, GenreSerializer, ReviewSerializer,
+    SignUpSerializer, TokenSerializer, UserSerializer,
+    TitleGETSerializer, TitleSerializer
+)
 
 
 class CDLViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
@@ -42,7 +47,7 @@ class CategoryViewSet(CDLViewSet):
     lookup_field = 'slug'
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (SearchFilter,)
     search_fields = ('=name',)
 
 
@@ -51,36 +56,35 @@ class GenreViewSet(CDLViewSet):
     lookup_field = 'slug'
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (SearchFilter,)
     search_fields = ('=name',)
 
 
 class TitleViewSet(ModelViewSet):
     permission_classes = (IsAdminOrReadOnly,)
-    queryset = Title.objects.all().annotate(rating=Avg('reviews__score'))
+    queryset = Title.objects.all().annotate(rating=Avg('reviews__score')).all()
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
     http_method_names = ('get', 'post', 'patch', 'delete')
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
-            return TitleReadSerializer
-        return TitleWriteSerializer
+            return TitleGETSerializer
+        return TitleSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    """ViewSet для работы с отзывами (Reviews)."""
-
     http_method_names = ['get', 'post', 'patch', 'delete']
     serializer_class = ReviewSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,
                           IsAuthorModeratorAdminOrReadOnly)
 
     def get_queryset(self):
-        return Review.objects.filter(title_id=self.kwargs['title_id'])
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
 
     def get_title(self):
-        return get_object_or_404(Title, id=self.kwargs['title_id'])
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
 
     def perform_create(self, serializer):
         title = self.get_title()
@@ -88,22 +92,22 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    """ViewSet для работы с комментариями к отзывам."""
-
     http_method_names = ['get', 'post', 'patch', 'delete']
     serializer_class = CommentSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,
                           IsAuthorModeratorAdminOrReadOnly)
 
     def get_review(self):
-        return get_object_or_404(
+        review = get_object_or_404(
             Review,
-            id=self.kwargs['review_id'],
-            title_id=self.kwargs['title_id']
+            id=self.kwargs.get('review_id'),
+            title_id=self.kwargs.get('title_id')
         )
+        return review
 
     def get_queryset(self):
-        return self.get_review().comments.all()
+        review = self.get_review()
+        return review.comments.all()
 
     def perform_create(self, serializer):
         review = self.get_review()
@@ -111,7 +115,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 class TokenObtainView(APIView):
-    """Получение JWT токена по username и confirmation_code."""
     permission_classes = (AllowAny,)
 
     def post(self, request, *args, **kwargs):
@@ -126,7 +129,6 @@ class TokenObtainView(APIView):
 
 
 class SignUpView(APIView):
-    """Регистрация нового пользователя или получение confirmation_code."""
     permission_classes = (AllowAny,)
 
     def post(self, request, *args, **kwargs):
@@ -137,17 +139,15 @@ class SignUpView(APIView):
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """ViewSet для управления пользователями (только для админов)."""
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAdmin,)
     lookup_field = 'username'
     http_method_names = ['get', 'post', 'patch', 'delete']
-    pagination_class = pagination.LimitOffsetPagination # Добавил пагинацию для пользователей
+    pagination_class = pagination.LimitOffsetPagination
 
     def get_queryset(self):
-        """Добавляем возможность поиска по username."""
-        queryset = super().get_queryset()
+        queryset = self.queryset
         search_term = self.request.query_params.get('search', None)
         if search_term:
             queryset = queryset.filter(username__icontains=search_term)
@@ -161,7 +161,6 @@ class UserViewSet(viewsets.ModelViewSet):
         url_name='users_me'
     )
     def me(self, request, *args, **kwargs):
-        """Получение и обновление данных своего профиля."""
         current_user = request.user
         if request.method == 'PATCH':
             serializer = self.get_serializer(
